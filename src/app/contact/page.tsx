24163d6d2d6badcc4fb13daf466c3ab5aeddf761
vlad medvede <vladmedvede@vlads-MacBook-Air.local>
2024-01-02 23:16:18 +0200

    mobile menu adding

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,106 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { useState } from "react";
+import { useRef } from "react";
+import { BASE_API_URL } from "@/utils/constants";
+
+// export const metadata = {
+//   title: "Contact Information",
+//   description: "This is contact page",
+// };
+
+const Contacts = () => {
+  const [sucess, setSucess] = useState("");
+  const [errors, setError] = useState("");
+  const [disable, setDisable] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const formRef = useRef(null);
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    try {
+      setDisable(true);
+      if (!formData.name) {
+        throw new Error("Please fill the form !");
+      }
+      const response = await fetch(`${BASE_API_URL}/api/email`, {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response) {
+        setError("");
+        setSucess("Email sent successfully!");
+        formRef.current?.reset();
+        setDisable(false);
+      } else {
+        setError("Email sending failed. Please try again later.");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="h-screen w-full bg-pink-50" data-testid="contact">
+      <div className="">
+        <h1 className="">Let's Keep in Touch</h1>
+      </div>
+      <h3 className="">{sucess ? sucess : null}</h3>
+      <h3 className="">{errors ? errors : null}</h3>
+
+      <div className="flex flex-col">
+        <div className="">
+          <Image className="" fill={true} src="" alt="contact image" />
+        </div>
+        <form
+          ref={formRef}
+          className=""
+          onSubmit={handleSubmit}
+          data-testid="form"
+        >
+          <input
+            type="text"
+            placeholder="name"
+            className=""
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="email"
+            className=""
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+          />
+          <textarea
+            className=""
+            name=""
+            id=""
+            cols={30}
+            rows={10}
+            placeholder="message"
+            onChange={(e) =>
+              setFormData({ ...formData, message: e.target.value })
+            }
+          />
+          <button disabled={disable} type="submit" className="">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contacts;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { VscAccount } from "react-icons/vsc";
 import CartItem from "./CartItem";
@@ -9,14 +9,24 @@ import { useDispatch } from "react-redux";
 import { removeAllProducts } from "@/redux/features/productsSlice";
 import { removeUserData } from "@/redux/features/userSlice";
 import { BASE_API_URL } from "@/utils/constants";
+import { FiMenu } from "react-icons/fi";
+import { IoIosClose } from "react-icons/io";
+import { IoHomeOutline } from "react-icons/io5";
+import { CiShop } from "react-icons/ci";
+import { CiShoppingCart } from "react-icons/ci";
+import { CiMail } from "react-icons/ci";
+
+import FormInput from "./Search";
 
 const navs = [
-  { name: "home", path: "/" },
-  { name: "products", path: "/products" },
-  { name: "cart", path: "/cart" },
+  { name: "home", path: "/", icon: <IoHomeOutline size={20} /> },
+  { name: "products", path: "/products", icon: <CiShop size={20} /> },
+  { name: "cart", path: "/cart", icon: <CiShoppingCart size={25} /> },
+  { name: "contact", path: "/contact", icon: <CiMail size={20} /> },
 ];
 
 const NavBar = () => {
+  const [isOpen, setOpen] = useState<Boolean>(false);
   const dispatch = useDispatch();
   const session = useSession();
   const router = useRouter();
@@ -27,54 +37,113 @@ const NavBar = () => {
     dispatch(removeUserData());
   }
 
-  return (
-    <div className="text-white sticky top-0 py-6 lg:px-[5vh] gap-7 bg-gradient-to-r from-purple-400 to-fuchsia-300 flex flex-col sm:flex-row items-center justify-around mb-10 z-20 rounded-b-md">
-      <Link href="/">
-        <h1 className="lg:text-4xl text-2xl font-bold">GadgetHub</h1>
-      </Link>
-      <div className="flex md:gap-20 gap-5  justify-center items-center md:text-sm text-xs">
-        {navs.slice(0, 2).map((item, i) => {
-          return (
-            <Link href={item.path} key={i} className="uppercase">
-              {item.name}
-            </Link>
-          );
-        })}
-        <div className="flex justify-center items-center">
-          <Link href="/cart" key={3} className="uppercase">
-            CART
-          </Link>
-          <Link href="/cart" key={4} className="uppercase">
-            <CartItem />
+  const MobileMenuIcon = () => {
+    return (
+      <div>
+        <div className="flex sm:hidden " onClick={() => setOpen(!isOpen)}>
+          {isOpen ? <IoIosClose size={30} /> : <FiMenu size={30} />}
+        </div>
+        <div></div>
+      </div>
+    );
+  };
+
+  const MobileMenu = () => {
+    return (
+      <div
+        className={`sm:hidden py-6 px-11 flex-col text-white h-screen flex  bg-gradient-to-r from-purple-400 to-fuchsia-300 w-full rounded-md`}
+      >
+        <div className="flex justify-center items-center mt-6">
+          <Link href="/">
+            <h1 className=" text-4xl font-bold">GadgetHub</h1>
           </Link>
         </div>
-        <div
-          className="flex gap-3 justify-center items-center"
-          onClick={() => {
-            session.status === "authenticated"
-              ? logOut()
-              : router.push("/login");
-          }}
-        >
-          <Link href="/login" key={3} className="uppercase">
-            {session.status === "authenticated" ? "Logout" : "Login"}
-          </Link>
-          <Link href="/login" key={4} className="uppercase">
-            {session.status === "authenticated" ? (
-              <div className="h-7 w-7">
-                {session?.data?.user?.image && (
-                  <img
-                    src={session.data.user.image}
-                    alt="avatar"
-                    className="rounded-full"
-                  />
-                )}
+        <div className="flex  gap-14 flex-col  h-full justify-center items-center">
+          {navs.slice(0, 4).map((item, i) => {
+            return (
+              <div className="flex shadow-lg p-3 gap-3 w-[70%] justify-start items-center rounded-lg bg-blue-300">
+                {item.icon}
+                <Link
+                  href={item.path}
+                  key={i}
+                  className="uppercase text-base"
+                  onClick={() => setOpen(false)}
+                >
+                  {item.name}
+                </Link>
               </div>
-            ) : (
-              <VscAccount size={25} />
-            )}
-          </Link>
+            );
+          })}
         </div>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <div className="text-white sticky  top-0 py-6 lg:px-[5vh] gap-7 bg-gradient-to-r from-purple-400 to-fuchsia-300 flex flex-col sm:flex-row items-center justify-around mb-10 z-20 rounded-b-md">
+        <div className="flex md:gap-6 gap-32 md:w-fit w-full md:justify-center items-center justify-around">
+          <Link href="/">
+            <h1 className="lg:text-4xl text-2xl font-bold">GadgetHub</h1>
+          </Link>
+          <MobileMenuIcon />
+        </div>
+        <div className="flex md:gap-20 gap-5  justify-center items-center md:text-sm text-xs">
+          {navs.slice(0, 2).map((item, i) => {
+            return (
+              <Link
+                href={item.path}
+                key={i}
+                className="uppercase hidden sm:block"
+              >
+                {item.name}
+              </Link>
+            );
+          })}
+
+          <div className="flex justify-center items-center">
+            <Link href="/cart" key={3} className="uppercase">
+              CART
+            </Link>
+            <Link href="/cart" key={4} className="uppercase">
+              <CartItem />
+            </Link>
+          </div>
+          <div
+            className="flex gap-3 justify-center items-center"
+            onClick={() => {
+              session.status === "authenticated"
+                ? logOut()
+                : router.push("/login");
+            }}
+          >
+            <Link href="/login" key={3} className="uppercase">
+              {session.status === "authenticated" ? "Logout" : "Login"}
+            </Link>
+            <Link href="/login" key={4} className="uppercase">
+              {session.status === "authenticated" ? (
+                <div className="h-7 w-7">
+                  {session?.data?.user?.image && (
+                    <img
+                      src={session.data.user.image}
+                      alt="avatar"
+                      className="rounded-full"
+                    />
+                  )}
+                </div>
+              ) : (
+                <VscAccount size={25} />
+              )}
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div
+        className={`fixed  z-50 shadow-2xl w-[85%] top-0 backdrop-blur-lg transition-all ease-in-out delay-300 duration-300 ${
+          isOpen ? "left-0" : "-left-full"
+        }`}
+      >
+        <MobileMenu />
       </div>
     </div>
   );
