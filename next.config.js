91107806afe3779bdb6911869f2051e0eecfb18c
vlad medvede <vladmedvede@vlads-MacBook-Air.local>
2024-02-17 20:05:14 +0200

    update config

--- next.config.js
+++ next.config.js
@@ -4,21 +4,28 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.example.com",
+        hostname: "**.gadstyle.com",
+        pathname: "**",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+        pathname: "**",
         port: "",
       },
     ],
-    domains: [
-      "via.placeholder.com",
-      "i.dummyjson.com",
-      "static.vecteezy.com",
-      "idme-marketplace.s3.amazonaws.com",
-      "www.gadstyle.com",
-      "img.freepik.com",
-      "cdn.dummyjson.com",
-      "i.ytimg.com",
-      "png.pngtree.com",
-    ],
+    // domains: [
+    //   "via.placeholder.com",
+    //   "i.dummyjson.com",
+    //   "static.vecteezy.com",
+    //   "idme-marketplace.s3.amazonaws.com",
+    //   "www.gadstyle.com",
+    //   "img.freepik.com",
+    //   "cdn.dummyjson.com",
+    //   "i.ytimg.com",
+    //   "png.pngtree.com",
+    // ],
   },
 };
 
