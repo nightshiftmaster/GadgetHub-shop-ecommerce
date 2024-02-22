ae5cd991399c2e4ac17db55c852b378700c1ab9c
vlad medvede <vladmedvede@vlads-MacBook-Air.local>
2024-02-22 03:07:13 +0200

    tests update

--- tests/e2e/test.spec.ts
+++ tests/e2e/test.spec.ts
@@ -380,7 +380,9 @@ test.describe("testing application", () => {
 
     await page.getByRole("button", { name: "Confirm order" }).click();
     await page.waitForTimeout(10000);
-    await page.waitForSelector('[data-testid="success-page"]');
+    await page.waitForSelector('[data-testid="success-page"]', {
+      timeout: 60000,
+    });
     // const currentUrl = page.url();
     // expect(currentUrl).toContain("success");
   });
